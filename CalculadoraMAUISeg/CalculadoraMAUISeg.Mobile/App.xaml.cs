10816afbd8d9656c026e2fe5fa6dcd9f183ea342
aalvarezm <andresalvarezm963@gmail.com>
2023-05-16 14:41:56 -0500

    Basic project creation

---
+++ CalculadoraMAUISeg/CalculadoraMAUISeg.Mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CalculadoraMAUISeg.Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
